df85f8cc571df56a2484692ec4d977f824632749
Maldon <maldonmeehan@gmail.com>
2016-08-12 06:22:40 -0700

    add working pipe

---
+++ app/healthy.pipe.ts
@@ -0,0 +1,26 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Pipe({
+  name: "healthy",
+  pure: false
+
+})
+export class HealthyPipe implements PipeTransform {
+  transform(mealList: Meal[], args) {
+    var userDropDownSelection = args[0];
+    if (userDropDownSelection === 'healthy'){
+      return mealList.filter((meal) =>{
+        // return meal.healthy;
+        return meal.calories <= 500;
+      });
+    } else if (userDropDownSelection === 'notHealthy'){
+        return mealList.filter((meal) =>{
+          // return !meal.healthy;
+          return meal.calories > 500;
+      });
+    } else {
+      return mealList;
+    }
+  }
+}
--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -3,14 +3,22 @@ import { MealComponent } from './meal.component';
 import { Meal } from './meal.model';
 import { EditMealDetailsComponent } from './edit-meal-details.component';
 import { NewMealComponent } from './new-meal.component';
+import {HealthyPipe} from './healthy.pipe';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
   outputs: ['onMealSelect'],
+  pipes: [HealthyPipe],
   directives: [MealComponent, EditMealDetailsComponent, NewMealComponent],
   template: `
-  <meal-display *ngFor="#currentMeal of mealList"
+  <label>Meal filter:</label>
+  <select (change)="onChange($event.target.value)">
+    <option value="all" selected="selected">All</option>
+    <option value="healthy">Under 500</option>
+    <option value="notHealthy">Over 500</option>
+  </select>
+  <meal-display *ngFor="#currentMeal of mealList | healthy:filterHealthy"
     (click)="mealClicked(currentMeal)"
     [class.selected]="currentMeal === selectedMeal"
     [meal]="currentMeal">
@@ -20,10 +28,12 @@ import { NewMealComponent } from './new-meal.component';
   <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
   `
 })
+
 export class MealListComponent {
   public mealList: Meal[];
   public onMealSelect: EventEmitter<Meal>;
   public selectedMeal: Meal;
+  public filterHealthy: string = "all";
   constructor() {
     this.onMealSelect = new EventEmitter();
   }
@@ -35,5 +45,8 @@ export class MealListComponent {
   createMeal(newMeal: Meal): void {
     newMeal.id = this.mealList.length;
     this.mealList.push(newMeal);
+  }
+  onChange(filterOptionByUserSelection) {
+  this.filterHealthy = filterOptionByUserSelection;
   }
 }
