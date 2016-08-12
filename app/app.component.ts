4e0ffab4c9bf109cdd88d09e55d81db78350a135
Maldon <maldonmeehan@gmail.com>
2016-08-11 18:00:06 -0700

    add working header display

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,1 +1,12 @@
-import { Component, EventEmitter } from 'angular2/core';
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Skeleton Angular2 App!</h1>
+  `
+})
+
+export class AppComponent {
+
+}
